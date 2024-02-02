66e8c31a46993a28e062a7452fcddeb233287ff1
snemoto-42 <115632171+snemoto-42@users.noreply.github.com>
2024-02-02 20:29:02 +0900

    0202-3

--- ex00/Convert.cpp
+++ ex00/Convert.cpp
@@ -3,6 +3,10 @@
 Convert::Convert(std::string arg):_arg(arg)
 {
 	std::cout << GREEN << "Convert : Default constructor called" << RESET << std::endl;
+	setChar();
+	setInt();
+	setFloat();
+	setDouble();
 }
 
 Convert::~Convert()
@@ -28,14 +32,42 @@ Convert& Convert::operator=(Convert const& x)
 
 std::string const& Convert::getArg(void) const {return _arg;}
 
+char const& Convert::getChar(void) const {return _c;}
+
+int const& Convert::getInt(void) const {return _i;}
+
+float const& Convert::getFloat(void) const {return _f;}
+
+double const& Convert::getDouble(void) const {return _d;}
+
+void Convert::setChar(void)
+{
+	_c = *_arg.c_str();
+}
+
+void Convert::setInt(void)
+{
+	_i = std::stoi(_arg);
+}
+
+void Convert::setFloat(void)
+{
+	_f = std::stof(_arg);
+}
+
+void Convert::setDouble(void)
+{
+	_d = std::stod(_arg);
+}
+
 std::ostream& operator<<(std::ostream& os, Convert const& ref)
 {
 	os << ">>>" << std::endl;
 	os << "original arg: " << ref.getArg() << std::endl;
-	os << "char: " << "xxx" << std::endl;
-	os << "int: " << "xxx" << std::endl;
-	os << "float: " << "xxx" << std::endl;
-	os << "double: " << "xxx" << std::endl;
+	os << "char: " << ref.getChar() << std::endl;
+	os << "int: " << ref.getInt() << std::endl;
+	os << "float: " << ref.getFloat() << std::endl;
+	os << "double: " << ref.getDouble() << std::endl;
 	os << "<<<";
 	return os;
 }
--- ex00/Convert.hpp
+++ ex00/Convert.hpp
@@ -15,10 +15,22 @@ class Convert
 		Convert & operator=(Convert const&);
 
 		std::string const& getArg(void) const;
+		char const& getChar(void) const;
+		int const& getInt(void) const;
+		float const& getFloat(void) const;
+		double const& getDouble(void) const;
+
+		void setChar(void);
+		void setInt(void);
+		void setFloat(void);
+		void setDouble(void);
 
 	private:
 		std::string _arg;
-
+		char _c;
+		int _i;
+		float _f;
+		double _d;
 };
 
 std::ostream & operator<<(std::ostream &, Convert const&);
